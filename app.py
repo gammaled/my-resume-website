e9a7afa38d5101ee87a1371f944efe34644d10ca
gammaled <guleduni@yahoo.co.uk>
2021-07-12 16:43:53 +0000

    fixed spacing so its PEP compliant

--- app.py
+++ app.py
@@ -54,8 +54,8 @@ def get_portfolio():
 def contact():
     return render_template("contact.html")
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"), 
             port=int(os.environ.get("PORT")),
             debug=True)
-            
